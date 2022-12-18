adaec1037be93c17d7377c62e996fecfe9b7ca7c
kiyosumi874 <shiiharakiyosumi@gmail.com>
2022-12-18 12:50:05 +0900

    SceneLoader仕様変更

--- Assets/MyAssets/Scripts/SceneLoader.cs
+++ Assets/MyAssets/Scripts/SceneLoader.cs
@@ -33,6 +33,13 @@ public static class SceneLoader
             {Scene.None, Error }
         };
 
+    private static Scene nowScene = Scene.None;
+
+    public static Scene GetNowScene()
+    {
+        return nowScene;
+    }
+
     public static void ChangeScene(Scene nextScene)
     {
         loadSceneActions[nextScene].Invoke();
@@ -40,46 +47,55 @@ public static class SceneLoader
 
     private static void ChangeToTitle()
     {
+        nowScene = Scene.Title;
         SceneManager.LoadScene("TitleScene");
     }
 
     private static void ChangeToSelect()
     {
+        nowScene = Scene.Select;
         SceneManager.LoadScene("SelectScene");
     }
 
     private static void ChangeToEasy()
     {
+        nowScene = Scene.Easy;
         SceneManager.LoadScene("EasyScene");
     }
 
     private static void ChangeToNormal()
     {
-        SceneManager.LoadScene("NormalScene");
+        nowScene = Scene.Normal;
+        SceneManager.LoadScene("EasyScene");
     }
 
     private static void ChangeToHard()
     {
-        SceneManager.LoadScene("HardScene");
+        nowScene = Scene.Hard;
+        SceneManager.LoadScene("EasyScene");
     }
 
     private static void ChangeToFailed()
     {
+        nowScene = Scene.Failed;
         SceneManager.LoadScene("Failed", LoadSceneMode.Additive);
     }
 
     private static void ChangeToSuccess()
     {
+        nowScene = Scene.Success;
         naichilab.RankingLoader.Instance.SendScoreAndShowRanking(100);
     }
 
     private static void ChangeToInstruction()
     {
+        nowScene = Scene.Instruction;
         SceneManager.LoadScene("Instruction", LoadSceneMode.Additive);
     }
 
     private static void Error()
     {
+        nowScene = Scene.None;
         Debug.LogAssertion("SceneChangerÇ≈éüÇÃÉVÅ[ÉìÇ™ëIÇŒÇÍÇƒÇ¢Ç‹ÇπÇÒ");
     }
 
