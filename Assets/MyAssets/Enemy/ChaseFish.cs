5782d4d8cf204e4dbaefbf2a5ddbc09514832aa5
Col <sj0063hv@ed.ritsumei.ac.jp>
2022-12-22 03:44:44 +0900

    誘導魚の実装(他verもつくる予定)

---
+++ Assets/MyAssets/Enemy/ChaseFish.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseFish : MonoBehaviour
+{
+    [SerializeField] private float chaseTime;
+    [SerializeField] private float speed;
+    [SerializeField] private float chaseDistance;
+    public GameObject player;
+    private Vector3 nowDistance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nowDistance = player.transform.position - transform.position;
+        if(nowDistance.y < chaseDistance)
+        {
+            //‹ß‚Ã‚­‚Æ’Ç‚Á‚Ä‚­‚é“G
+            transform.Translate(nowDistance/1000 * speed);
+        }
+    }
+}
